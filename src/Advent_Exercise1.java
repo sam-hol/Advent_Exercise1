21cb33769d6a69e41b1ffe6f5648fc0ea4874e38
samhol <sam.hollinger@proton.me>
2022-08-08 23:26:51 -0400

    updated with part 2

--- src/Advent_Exercise1.java
+++ src/Advent_Exercise1.java
@@ -7,14 +7,16 @@ public class Advent_Exercise1 {
         try {
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
             ArrayList<Integer> depths = new ArrayList();
+            ArrayList<Integer> windows = new ArrayList();
             String num;
+            int triplet = 0;
             int dIncrease = 0;
             int previous = 0;
 
             while ((num = br.readLine()) != null) {
                 depths.add(Integer.parseInt(num));
             }
-
+            /*
             previous = depths.get(0);
             for(int n:depths) {
                 if(n > previous) {
@@ -22,6 +24,21 @@ public class Advent_Exercise1 {
                 }
                 previous = n;
             };
+             */
+            for(int i = 0; i < depths.size(); i++) {
+                triplet = depths.get(i);
+                if(i <= (depths.size() - 3)) {
+                    triplet = triplet + depths.get(i + 1) + depths.get(i + 2);
+                } else break;
+                windows.add(triplet);
+            }
+            previous = windows.get(0);
+            for(int n:windows) {
+                if(n > previous) {
+                    dIncrease++;
+                }
+                previous = n;
+            }
         System.out.println("Number of depth increases: " + dIncrease);
 
         } catch(Exception e) {
